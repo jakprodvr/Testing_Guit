13d8cedf7eb8298e23d3165f86abb6782284ef73
jakingson <jakingsonmm@gmail.com>
2021-11-26 07:45:19 -0500

    adding comment

--- Testing_Guit/Program.cs
+++ Testing_Guit/Program.cs
@@ -4,6 +4,10 @@ namespace Testing_Guit
 {
     class Program
     {
+        /// <summary>
+        /// this method write in console 'hello word'
+        /// </summary>
+        /// <param name="args"> list of parameter</param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
